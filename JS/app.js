e554f21473ce4656098c0ecf58db1de5b39954a9
hvb <helene.vanbesouw@gmail.com>
2023-04-01 09:18:17 +0100

    form submission , saving projects list to local storage

--- JS/app.js
+++ JS/app.js
@@ -1,22 +1,48 @@
 const projectsContainer = document.querySelector('[data-projects]')
+const projectForm = document.querySelector('[data-new-project-form]')
+const projectName = document.querySelector('[data-new-project-name]')
 
-let projects = ['projectOne', 'projectTwo', 'projectThree', 'project4', 'project5', 'project6']
 
-function projectList(){
+const LOCAL_STORAGE_PROJECT_LIST = 'projects.list'
 
+let projects = JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROJECT_LIST)) || []
+
+function projectDisplay(){
+     clearProject(projectsContainer)
     projects.forEach(project =>{
         const projectElement = document.createElement('div')
         const projectHeading = document.createElement('h3')
-        projectHeading.innerText = project
+        projectHeading.innerText = project.name
         projectElement.appendChild(projectHeading)
         projectElement.classList.add("project-element")
         projectsContainer.appendChild(projectElement)
-
-    
+        console.log(project)
 
     })
    
 }
 
+projectForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const newProject = projectName.value
+    // console.log(newProject)
+    if(newProject === null |newProject === "") return
+   const projectList = createProject(newProject)
+   projectName.value = null 
+   projects.push(projectList)
+   localStorage.setItem(LOCAL_STORAGE_PROJECT_LIST, JSON.stringify(projects))
+   projectDisplay()
 
-projectList()
+})
+
+function createProject(name){
+    return {id: Date.now().toString(), name: name , task:[]}
+
+}
+
+function clearProject(element){
+    while (element.firstChild)
+    element.removeChild(element.firstChild)
+}
+
+projectDisplay()
