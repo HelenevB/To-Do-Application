59ed306279c30ae0d64092047c8f96522ff84da1
hvb <helene.vanbesouw@gmail.com>
2023-04-07 14:30:11 +0100

    add task function , details logging to console

--- JS/app.js
+++ JS/app.js
@@ -1,17 +1,22 @@
 const projectsContainer = document.querySelector('[data-projects]')
 const projectForm = document.querySelector('[data-new-project-form]')
 const projectName = document.querySelector('[data-new-project-name]')
+const taskName = document.querySelector('[data-new-task-name]')
+const addTaskForm =document.querySelector('[data-new-task]')
+const addTask = document.getElementById("add-Task")
+const displayProject = document.getElementById("project-details")
+const projectTitle =document.getElementById("project-title")
 
 
 
 const LOCAL_STORAGE_PROJECT_LIST = 'projects.list'
 
 let projects = JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROJECT_LIST)) || []
+console.log(projects)
 
-
+window
 function projectDisplay(){
  clearProject(projectsContainer);
-
  projects.forEach((project) => {
    const projectElement = document.createElement('div');
    const projectHeading = document.createElement('h3');
@@ -39,8 +44,6 @@ function projectDisplay(){
    projectCreated.addEventListener('click', displayDetails);
  });
 }
-
-
 
 
 function deleteProject(event){
@@ -72,9 +75,26 @@ function createProject(name){
     const date = new Date()
     const options = { timeZone: 'UTC', day: '2-digit', month: '2-digit', year: '2-digit' };
     const formatDate= date.toLocaleString('en-UK', options).replace(',', '/');
-    return {id: Date.now().toString(), name: name , createdDate: formatDate,task:[]}
+    return {id: Date.now().toString(), name: name , createdDate: formatDate ,task:[]}
 
 }
+
+
+
+addTaskForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const currentProjectId = displayProject.getAttribute('data-project-identifier')
+    const currentProject = projects.findIndex((project)=> project.id === currentProjectId)
+    console.log(currentProject)
+    const newTask = taskName.value
+    if(!taskName) return;
+    projects[currentProject].task.push(newTask)
+    taskName.value = null
+    localStorage.setItem(LOCAL_STORAGE_PROJECT_LIST, JSON.stringify(projects))
+    console.log(projects[currentProject].task)
+
+})
+
 
 function clearProject(element){
     while (element.firstChild)
@@ -83,12 +103,16 @@ function clearProject(element){
 
 
 function displayDetails(event){
-   projects.map((project) =>{
-    if(project.id === event.target.getAttribute('data-project-identifier')){
-        console.log(`${project.name},${project.id}, ${project.createdDate},${project.task}`)
-    }
+   projects.forEach((project)=>{
+    if(project.id ===event.target.getAttribute('data-project-identifier')){
+        console.log(project.name)
+        projectTitle.innerText = project.name
+        displayProject.setAttribute('data-project-identifier', project.id)
 
+    }
    })
+
+
 }
 
 projectDisplay()
